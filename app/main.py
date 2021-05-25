57966214f04de423fea7e1e416a91bcb801d51fd
EC2 Default User <ec2-user@ip-10-0-0-116.ap-northeast-1.compute.internal>
2021-05-25 05:44:09 +0000

    v1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from mangum import Mangum
 
 from api.v1.api import router as api_router
 
-app = FastAPI(title='Serverless Lambda FastAPI')
+app = FastAPI(title='Serverless Lambda FastAPI V1')
 
 app.include_router(api_router, prefix="/api/v1")
 
